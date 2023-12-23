43e5a525f7772b0fed957b7d4d3b6a06c5c75ffc
RidwanRizqi <Ridwanrizqi64@gmail.com>
2023-12-23 11:03:33 +0700

    feat : upload image files

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:scan_cv/preview_page.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:image_picker/image_picker.dart';
 
 class CameraPage extends StatefulWidget {
   final List<CameraDescription>? cameras;
@@ -38,7 +39,7 @@ class _CameraPageState extends State<CameraPage> {
 
   Future<dynamic> scanCv(XFile picture) async {
     var request = http.MultipartRequest(
-        'POST', Uri.parse('http://192.168.1.2:5000/extract_info'));
+        'POST', Uri.parse('https://ridwanrizqi64.pythonanywhere.com/extract_info'));
 
     request.files.add(http.MultipartFile.fromBytes(
         'file', File(picture.path).readAsBytesSync(),
@@ -48,11 +49,10 @@ class _CameraPageState extends State<CameraPage> {
     var response = await http.Response.fromStream(res);
 
     var tes = json.decode(response.body);
-    // print(tes['Email']);
     return tes;
   }
 
-  Future takePicture() async {
+  Future<void> takePicture() async {
     if (!_cameraController.value.isInitialized) {
       return null;
     }
@@ -62,20 +62,30 @@ class _CameraPageState extends State<CameraPage> {
     try {
       await _cameraController.setFlashMode(FlashMode.off);
       XFile picture = await _cameraController.takePicture();
-      var extractedData = await scanCv(picture);
-      // ignore: use_build_context_synchronously
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => PreviewPage(
-            picture: picture,
-            data: extractedData,
-          ),
-        ),
-      );
+      await processPicture(picture);
     } on CameraException catch (e) {
-      debugPrint('Error occured while taking picture: $e');
-      return null;
+      debugPrint('Error occurred while taking a picture: $e');
+    }
+  }
+
+  Future<void> processPicture(XFile picture) async {
+    var extractedData = await scanCv(picture);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PreviewPage(
+          picture: picture,
+          data: extractedData,
+        ),
+      ),
+    );
+  }
+
+  Future<void> pickImageFromGallery() async {
+    final ImagePicker _picker = ImagePicker();
+    XFile? galleryImage = await _picker.pickImage(source: ImageSource.gallery);
+    if (galleryImage != null) {
+      await processPicture(galleryImage);
     }
   }
 
@@ -95,56 +105,69 @@ class _CameraPageState extends State<CameraPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Stack(children: [
-          (_cameraController.value.isInitialized)
-              ? CameraPreview(_cameraController)
-              : const Center(
-                  child: CircularProgressIndicator(),
+        child: Stack(
+          children: [
+            (_cameraController.value.isInitialized)
+                ? CameraPreview(_cameraController)
+                : const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.2,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+                  color: Colors.black,
                 ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.2,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
-                color: Colors.black,
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: IconButton(
-                      padding: EdgeInsets.zero,
-                      iconSize: 30,
-                      icon: Icon(
-                        _isRearCameraSelected
-                            ? CupertinoIcons.switch_camera
-                            : CupertinoIcons.switch_camera_solid,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: IconButton(
+                        padding: EdgeInsets.zero,
+                        iconSize: 30,
+                        icon: Icon(
+                          _isRearCameraSelected
+                              ? CupertinoIcons.switch_camera
+                              : CupertinoIcons.switch_camera_solid,
+                        ),
+                        color: Colors.white,
+                        onPressed: () {
+                          setState(() =>
+                              _isRearCameraSelected = !_isRearCameraSelected);
+                          initCamera(
+                              widget.cameras![_isRearCameraSelected ? 0 : 1]);
+                        },
                       ),
-                      color: Colors.white,
-                      onPressed: () {
-                        setState(() =>
-                            _isRearCameraSelected = !_isRearCameraSelected);
-                        initCamera(
-                            widget.cameras![_isRearCameraSelected ? 0 : 1]);
-                      },
                     ),
-                  ),
-                  Expanded(
-                    child: IconButton(
-                      onPressed: takePicture,
-                      iconSize: 50,
-                      padding: EdgeInsets.zero,
-                      constraints: const BoxConstraints(),
-                      icon: const Icon(Icons.circle, color: Colors.white),
+                    Expanded(
+                      child: IconButton(
+                        onPressed: takePicture,
+                        iconSize: 50,
+                        padding: EdgeInsets.zero,
+                        constraints: const BoxConstraints(),
+                        icon: const Icon(Icons.circle, color: Colors.white),
+                      ),
                     ),
-                  ),
-                  const Spacer(),
-                ],
+                    Expanded(
+                      child: IconButton(
+                        onPressed: pickImageFromGallery,
+                        iconSize: 30,
+                        padding: EdgeInsets.zero,
+                        icon: Icon(
+                          Icons.image,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                  ],
+                ),
               ),
-            ),
-          )
-        ]),
+            )
+          ],
+        ),
       ),
     );
   }
